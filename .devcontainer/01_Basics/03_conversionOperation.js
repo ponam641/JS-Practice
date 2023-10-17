633778ab78a9e1a081418e9693dcd45c4240de73
ponam641 <62901888+ponam641@users.noreply.github.com>
2023-10-17 20:42:58 +0000

    conversion done

---
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true
+let score1 = Number(score)
+
+console.log(typeof(score1));
+console.log(score1);
+
+
+// conversion:
+// "33" => 33
+// "33advdv" => NaN (Not A Number)
+// true => 1 false => 0
+
+
+let isLoggedIn = "finbfbs"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanLoggedIn)
+
+// 1=> true 0 => false
+// "" => false
+// "fobf" => true
+
+
+let someNumber = 25
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
