7ec3e371171db8df9aa13beb154b0523cf1e9bd7
ponam641 <62901888+ponam641@users.noreply.github.com>
2023-10-17 19:45:22 +0000

    container config done

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 151515
+let accountEmail = "ponam641@gmail.com"
+accountCity = "Ambala"
+
+console.table([accountId, accountEmail, accountCity ])
+
+/* Prefer not to use var keyword 
+because of issue in block scope and functional scope 
+*/
