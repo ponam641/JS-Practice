23d8eecf106c24ba3ed5ceb6583f763bb24ae633
ponam641 <62901888+ponam641@users.noreply.github.com>
2023-10-17 20:12:50 +0000

    datatype

---
+++ .devcontainer/01_Basics/02_datatype.js
@@ -0,0 +1,16 @@
+// datatypes:
+
+// Primitive datatypes: numbers, bigInt, string, boolean, null, undefined, symbol
+// number => 2 the power of 53
+// string => use ""
+// bigInt => used on big platforms like stockmarket or fb etc.
+// boolean =>true/false
+// null => standalone value. It is a representtion of empty value.
+// undefined => value not assigned/defined
+// symbol => to find uniqueness. Mostly used in ReactJS.
+
+
+// Objects
+
+console.log(typeof null);
+console.log(typeof undefined);
