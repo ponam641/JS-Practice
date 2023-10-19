835293e93eb60f6185c495fc8b17745e4cb47e32
ponam641 <62901888+ponam641@users.noreply.github.com>
2023-10-19 20:16:03 +0000

    date in js learned

---
+++ .devcontainer/01_Basics/07_dateinjs.js
@@ -0,0 +1,12 @@
+// Dates:
+const myDate = new Date()
+console.log(myDate);
+console.log(myDate.getDate());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toUTCString());
+
+let createdDate = new Date (2023,3,21)
+console.log(createdDate.toLocaleDateString());
