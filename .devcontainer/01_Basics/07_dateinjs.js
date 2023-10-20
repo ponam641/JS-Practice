40669a2f0082621db8fb597daeae9ab674c023d5
ponam641 <62901888+ponam641@users.noreply.github.com>
2023-10-20 21:00:11 +0000

    date learned complete

--- .devcontainer/01_Basics/07_dateinjs.js
+++ .devcontainer/01_Basics/07_dateinjs.js
@@ -8,5 +8,25 @@ console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toUTCString());
 
-let createdDate = new Date (2023,3,21)
-console.log(createdDate.toLocaleDateString());
+let createdDate = new Date (2023,3,21,15,6)
+console.log(createdDate.toLocaleString());
+let createdDate1 = new Date ("2023-3-21")
+console.log(createdDate1.toLocaleString());
+let createdDate2 = new Date ("02-25-2023")
+console.log(createdDate2.toLocaleString());
+
+// Time Stamps
+
+let newTime = Date.now()
+console.log(newTime);
+// in above we get time in milliseconds from since 1970 to till time.
+console.log(createdDate2.getTime()-newTime);
+console.log(Math.floor(Date.now()/1000));
+// above is used to convert millseconds into seconds.
+
+// we can also customize the display of date-time, using below:
+
+myDate.toLocaleString("default", {
+    month:"long",
+    dateStyle:"full",
+})
