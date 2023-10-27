ebb0d3836b5c3b92402c4acff887630080b96dae
ponam641 <62901888+ponam641@users.noreply.github.com>
2023-10-27 18:41:04 +0000

    function with objects and array learned

--- .devcontainer/03_Basics/01_functions.js
+++ .devcontainer/03_Basics/01_functions.js
@@ -37,3 +37,47 @@ function userLoginMsg(username="New Buddy") {
     return `${username} just logged in.`
 }
 userLoginMsg();
+
+// using "rest operator (i.e., ...)" we can pass any number of parameters in the function. Eg.
+function calculateCardPrice(...numb1){
+    return numb1
+}
+console.log(calculateCardPrice(2,85,36,9000,1600));
+
+// but if we pass some arguments and then apply rest operator then some parameters will be kept under variables and rest within an array. Eg
+function calculateCartPrice(val1, val2, ...numb1){
+    return numb1
+}
+console.log(calculateCartPrice(2,85,36,9000,1600));
+
+// how to access objects in functions
+
+const user ={
+    itemname: "Winter wear",
+    price: 9000
+}
+
+function handleObjectCart(anyobject) {
+    console.log(`${anyobject.itemname} is the item ordered having price @ ${anyobject.price} dollars`);
+}
+handleObjectCart(user)
+
+// instead of creating object & then passing it as parameter we can directly create object while calling function. Eg.
+
+handleObjectCart({
+    itemname: "Women Jeans",
+    price: 3000
+})
+
+// How to pass or access Arrays in function
+
+const myNewArray =[200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[0]
+}
+
+console.log(returnSecondValue(myNewArray));
+
+// Similarly as in object, instead of creating an array & then passing it as parameter we can directly create aray while calling function. Eg.
+
+console.log(returnSecondValue([600,100,200,800]));
