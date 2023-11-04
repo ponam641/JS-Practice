b1592fc0ea470a3755a30d651d53a209d34f85ae
ponam641 <62901888+ponam641@users.noreply.github.com>
2023-11-04 21:33:16 +0000

    while & do-while loop talks

---
+++ .devcontainer/05_iteration/two.js
@@ -0,0 +1,23 @@
+// while loop
+let index = 0
+while (index <=10) {
+    console.log(`value of index is ${index}`);
+    index= index+2
+}
+
+// while loop with array
+
+let myArray = ["Shaktiman", "Alif-Lella", "Alladin-Ka-Chirag", "JuniorG"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`My all time favt. show is ${myArray[arr]}`);
+    arr++
+}
+
+
+// do-while loop
+let score = 1
+do {
+    console.log(`current score is ${score}`);
+    score++
+} while (score<=10);
